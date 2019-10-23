c5298e29f4237e6acd641d647b979786e0297730
Mike Cooper <mikescooper@icloud.com>
2019-10-23 04:55:08 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,9 +1,15 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year FROM books WHERE books.series_id = 1 ORDER BY books.year;"
+  "SELECT books.title, books.year 
+  FROM books 
+  WHERE books.series_id = 1 
+  ORDER BY books.year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1;"
+  "SELECT name, motto 
+  FROM characters 
+  ORDER BY LENGTH(motto) DESC 
+  LIMIT 1;"
 end
 
 
@@ -16,7 +22,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT"
 end
 
 def select_series_title_with_most_human_characters
