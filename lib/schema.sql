43a60715a7ef86d1b7031ceee90bfdd1bc6eda48
Mike Cooper <mikescooper@icloud.com>
2019-10-23 04:35:29 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -2,10 +2,10 @@ CREATE TABLE series (
 id INTEGER PRIMARY KEY,
 title TEXT,
 author_id INTEGER,
-sug_genre_id INTEGER 
+subgenre_id INTEGER 
 );
 
-CREATE TABLE sub_genres (
+CREATE TABLE subgenres (
 id INTEGER PRIMARY KEY,
 name TEXT 
 );
