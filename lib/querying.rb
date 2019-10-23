5055dc0e3057610ecb1792d2f1566da1cb369f89
Mike Cooper <mikescooper@icloud.com>
2019-10-23 04:59:35 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,7 +27,7 @@ def select_name_and_series_subgenres_of_authors
   JOIN series 
   ON series.author_id = authors.id 
   JOIN subgenres
-  ON series.subgenre_id = subgenre.id;"
+  ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
